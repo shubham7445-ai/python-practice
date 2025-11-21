ee08e66315573ce6d1ea31545790dc64f64faa8d
shubham7445-ai <gaikwadshubham9559@gmail.com>
2025-11-21 12:42:35 +0530

    Add factorial function implementation

---
+++ functions/factorial_function.py
@@ -0,0 +1,14 @@
+# Program to find factorial of a number using function
+
+def factorial(n):
+    result = 1
+    for i in range(1, n + 1):
+        result *= i
+    return result
+
+num = int(input("Enter a non-negative integer: "))
+
+if num < 0:
+    print("Factorial is not defined for negative numbers.")
+else:
+    print("Factorial of", num, "is", factorial(num))
